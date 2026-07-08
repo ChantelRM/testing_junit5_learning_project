17902b763740e4a9a437d3f4e23ba7dd1a2819f5
Chantel Muthaphuli <chantelmuthaphuli@gmail.com>
2026-07-08 12:33:43 +0200

    test: added filtering test class

---
+++ fintech-bank/src/test/java/com/fintech/model/TransactionFilterTest.java
@@ -0,0 +1,45 @@
+package com.fintech.model;
+
+import org.junit.jupiter.api.Test;
+
+public class TransactionFilterTest {
+    @Test
+    void getTransactionsByType_shouldReturnOnlyDeposits(){
+
+    }
+
+    @Test
+    void getTransactionsByType_shouldReturnOnlyWithdrawals(){
+
+    }
+
+    @Test
+    void getTransactionsByType_whenTypeNotPresent_shouldReturnEmptyList(){
+
+    }
+
+    @Test
+    void getTransactionsByMonth_shouldReturnOnlyTransactionsFromThatMonth(){
+
+    }
+
+    @Test
+    void getTransactionsByMonth_whenNoTransactionsInMonth_shouldReturnEmptyList(){
+
+    }
+
+    @Test
+    void getTransactionsBetween_shouldReturnTransactionsWithinRange(){
+
+    }
+
+    @Test
+    void getTransactionsBetween_shouldExcludeTransactionsOutsideRange(){
+
+    }
+
+    @Test
+    void getTransactionsBetween_whenFromAndToAreSame_shouldReturnEmptyList(){
+
+    }
+}
